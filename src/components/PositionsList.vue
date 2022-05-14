64e4ea74a80bedcfa0f2ffe98b410eefbb819b27
Sultan Mustafin <sultan.fb29@gmail.com>
2022-05-14 23:02:25 +0300

    feat: create home page and main components

---
+++ src/components/PositionsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="list">
+    <li class="list__item" v-for="position in positionsList">{{ position }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const positionsList = ref([
+  "Web developer",
+  "Bussines school student",
+  "Caffeine addict",
+]);
+</script>
+
+<style scoped>
+.list {
+  font-size: 16px;
+  padding-left: 20px;
+  margin-top: 40px;
+  font-weight: 400;
+}
+
+.list__item:hover {
+  cursor: pointer;
+  color: var(--color-text-highlighted);
+  transition: color 0.1s linear;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <Heading>Sultan Mustafin</Heading>
+    <PositionsList />
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import Heading from "../components/common/Heading.vue";
+import PositionsList from "../components/PositionsList.vue";
+</script>
+
+<style scoped>
+.main {
+  width: 100vw;
+  padding: 50px;
+}
+</style>
