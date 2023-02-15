175fd537a34be89cb7ed0e2a4d5fb7feffac895f
Sultan Mustafin <sultan.fb29@gmail.com>
2023-02-15 09:46:58 +0300

    feat: create screen background

--- src/views/GlitchedView/GlitchedView.vue
+++ src/views/GlitchedView/GlitchedView.vue
@@ -2,17 +2,22 @@
 
 <template>
   <main class="main">
-    <div class="main__masks">
-      <img
-        src="../../assets/gifs/background-glitches.gif"
-        class="main__mask"
-        :style="{ opacity: 0.4 }"
-      />
-      <img
-        src="../../assets/gifs/glitch-background-full.gif"
-        class="main__mask"
-      />
-    </div>
+    <pre>
+      <div class="main__masks">
+        <img src="../../assets/gifs/effect-static.webp" class="main__mask" />
+        <div class="main__mask gradient"></div>
+        <div class="main__mask"></div>
+        <div class="main__mask"></div>
+      </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint velit nisi
+        beatae aliquid et accusantium perferendis repellendus, rem, non, facere id
+        dolorem deserunt ea temporibus ipsam eos inventore sit officia architecto.
+        Dolore mollitia fuga iusto quos earum magnam beatae quod modi, eaque soluta
+        dolorum, tempora molestias nam fugiat odit culpa assumenda asperiores eum,
+        minus quasi nesciunt veniam consectetur. Ex.
+      </p>
+    </pre>
   </main>
 </template>
 
@@ -25,6 +30,34 @@
   top: 0;
   left: 0;
 
-  opacity: 0.2;
+  opacity: 0.02;
+}
+
+.gradient {
+  background-image: repeating-linear-gradient(
+    transparent,
+    transparent 2px,
+    hsla(0, 0%, 100%, 0.078) 3px,
+    hsla(0, 0%, 100%, 0.077) 3px,
+    hsla(236, 100%, 50%, 0.078) 4px,
+    hsla(236, 100%, 50%, 0.077) 4px,
+    hsla(303, 100%, 50%, 0.078) 6px,
+    hsla(303, 100%, 50%, 0.077) 6px
+  );
+  opacity: 1;
+}
+
+pre {
+  animation: text-blink 0.01s steps(2) infinite;
+}
+
+@keyframes text-blink {
+  from,
+  to {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
 }
 </style>
