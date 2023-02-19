0866c1ac930440db54e4c93b73e4974f28bb151e
Sultan Mustafin <sultan.fb29@gmail.com>
2023-02-19 06:41:08 +0300

    feat: set up basic terminal intro

--- src/components/Terminal/TerminalIntro/TerminalIntro.vue
+++ src/components/Terminal/TerminalIntro/TerminalIntro.vue
@@ -2,7 +2,7 @@
 import GlitchedWriter from "vue-glitched-writer";
 
 const message =
-  "SMZ industries internal protocol Remote terminal connection obtained";
+  "SMZ industries internal remote terminal connection obtained <br> From: <span style='color: green'>Turkey</span> Local Time: <span style='color: green'>06:37</span>";
 </script>
 
 <template>
@@ -10,6 +10,7 @@ const message =
     <GlitchedWriter
       :text="message"
       :options="{
+        html: true,
         interval: [25, 40],
         delay: [0, 0],
         steps: 0,
@@ -26,5 +27,3 @@ const message =
     />
   </div>
 </template>
-
-<style scoped></style>
